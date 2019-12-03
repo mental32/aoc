c948f24f8bd8ba9d9afb2f749363368f6b453a8d
mental <m3nta1@yahoo.com>
2019-12-03 15:27:21 +0000

    Update solution.hs

--- day_3/solution.hs
+++ day_3/solution.hs
@@ -1,20 +1,12 @@
 import System.IO (withFile, hGetContents, IOMode ( ReadMode ))
-import Data.List.Split as Sp (splitOn)
+import qualified Data.List.Split as Sp (splitOn)
 import qualified Data.Set as S (intersection, map, findMin, fromList, toList)
+import qualified Data.Map as M (fromList)
 
 type Point = (Integer, Integer)
 
-fooX :: Char -> Integer
-fooX 'L' = -1
-fooX 'R' = 1
-fooX 'U' = 0
-fooX 'D' = 0
-
-fooY :: Char -> Integer
-fooY 'L' = 0
-fooY 'R' = 0
-fooY 'U' = 1
-fooY 'D' = -1
+fooX = M.fromList $ zip "LRUD" (-1, 1, 0, 0)
+fooY = M.fromList $ zip "LRUD" (0, 0, 1, -1)
 
 points :: Point -> [String] -> [Point]
 points p []          = []
@@ -25,8 +17,8 @@ points' :: Char -> Integer -> Point -> [Point]
 points' _ 0 p = [p]
 points' d n (x, y) = (points' d (n - 1) (x', y')) ++ [(x', y')]
     where
-        x' = x + (fooX d)
-        y' = y + (fooY d)
+        x' = x + ((M.!) fooX d)
+        y' = y + ((M.!) fooY d)
 
 calc :: String -> String -> Integer
 calc these those = S.findMin . (S.map (uncurry (+))) $ (points'' these) `S.intersection` (points'' those)
@@ -36,4 +28,4 @@ main :: IO ()
 main = withFile
     "input_data" ReadMode (\handle -> do
         contents <- hGetContents handle
-        let (x:y:_) = take 2 $ lines contents in print $ calc x y)
+        let (this:that:_) = take 2 $ lines contents in print $ calc this that)
