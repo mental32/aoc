d3cf970b72573833ed8606de0e206ad5382206ac
mental <m3nta1@yahoo.com>
2019-12-08 06:03:02 +0000

    Complete day eight part two

--- day_8/solution.py
+++ day_8/solution.py
@@ -11,3 +11,28 @@ assert any("0" in layer for layer in layers)
 
 smallest = min(layers, key=(lambda layer: layer.count("0")))
 print(smallest.count("1") * smallest.count("2"))
+
+from blessings import Terminal
+
+terminal = Terminal()
+
+final = []
+
+with terminal.fullscreen():
+    print(terminal.clear)
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            for layer in layers:
+                pixel = layer[y * WIDTH + x]
+                if pixel == "2":
+                    # Transparent
+                    continue
+
+                with terminal.location(x, y):
+                    if pixel == "1":
+                        print("*", end="", flush=True)
+                    else:
+                        print(" ", end="", flush=True)
+                break
+        print("", flush=True, end="")
+    input()  # Needed as exit of fullscreen clears the screen.
