3170d0bfdb587a466d1a8bcc3114ffc279a4f6a7
mental <m3nta1@yahoo.com>
2019-12-08 06:52:21 +0000

    Tidy up the solution.

--- day_8/solution.py
+++ day_8/solution.py
@@ -1,3 +1,5 @@
+from blessings import Terminal
+
 with open("input_data") as file:
     data = file.read().strip()
 
@@ -7,32 +9,28 @@ RES = WIDTH * HEIGHT
 
 layers = [data[i:i + RES] for i in range(0, len(data), RES)]
 
+# Part 1
+
 assert any("0" in layer for layer in layers)
 
 smallest = min(layers, key=(lambda layer: layer.count("0")))
 print(smallest.count("1") * smallest.count("2"))
 
-from blessings import Terminal
+# Part 2
 
 terminal = Terminal()
-
-final = []
 
 with terminal.fullscreen():
     print(terminal.clear)
     for y in range(HEIGHT):
         for x in range(WIDTH):
-            for layer in layers:
-                pixel = layer[y * WIDTH + x]
-                if pixel == "2":
-                    # Transparent
-                    continue
+            pixel = next(layer[y * WIDTH + x] for layer in layers if layer[y * WIDTH + x] != "2")
 
-                with terminal.location(x, y):
-                    if pixel == "1":
-                        print("*", end="", flush=True)
-                    else:
-                        print(" ", end="", flush=True)
-                break
+            with terminal.location(x, y):
+                if pixel == "1":
+                    print(terminal.on_white(" "), end="", flush=True)
+                else:
+                    print(terminal.on_black(" "), end="", flush=True)
+
         print("", flush=True, end="")
-    input()  # Needed as exit of fullscreen clears the screen.
+
