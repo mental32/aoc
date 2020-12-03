a56aebc654fd6c1fb7e5399b9bc1270bf1326951
mental <m3nta1@yahoo.com>
2020-12-03 07:50:46 +0200

    Add day 3

---
+++ 2020/day_3/three.py
@@ -0,0 +1,48 @@
+from itertools import *
+from functools import *
+from typing import *
+from math import *
+from pathlib import *
+
+p = Path(__file__).parent.joinpath("three.txt").absolute()
+
+assert p.exists()
+
+input_data = p.read_text().strip().split("\n")
+row_length = len(input_data[0])
+
+def solve(delta, start = (0, 0)) -> int:
+    dx, dy, = delta
+    x, y, = start
+
+    hits = 0
+
+    while True:
+        try:
+            hits += input_data[y][x] == '#'
+        except IndexError:
+            return hits
+
+        x += dx % row_length
+        y += dy
+
+
+def part1() -> int:
+    return solve((3, 1), (3, 1)) # part 1
+
+
+def part2() -> int:
+    p2 = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+    n = 1
+
+    for delta in p2:
+        t = solve(delta)
+        n *= t
+
+    return n
+
+from timeit import timeit
+
+print(f"Part1 -> {timeit('part1()', globals=globals())}")
+print(f"Part2 -> {timeit('part2()', globals=globals())}")
